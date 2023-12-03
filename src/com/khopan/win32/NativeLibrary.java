eded56d028436d7c5ad699077daf8dc49e2f7d06
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-12-03 12:44:56 +0700

    Change Library Path

--- src/com/khopan/win32/NativeLibrary.java
+++ src/com/khopan/win32/NativeLibrary.java
@@ -1,12 +1,8 @@
 package com.khopan.win32;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-
 class NativeLibrary {
 	static {
-		try {
+		/*try {
 			InputStream stream = NativeLibrary.class.getResourceAsStream("Win32.dll");
 			FileOutputStream output = new FileOutputStream(new File("C:\\Win32.dll"));
 			output.write(stream.readAllBytes());
@@ -15,7 +11,9 @@ class NativeLibrary {
 			System.load("C:\\Win32.dll");
 		} catch(Throwable Errors) {
 			throw new RuntimeException(Errors);
-		}
+		}*/
+
+		System.load("D:\\GitHub Repository\\Win32\\WindowsAPI\\x64\\Debug\\WindowsAPI.dll");
 	}
 
 	private NativeLibrary() {}
